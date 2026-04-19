94acb895f77ea0523814a53a80d9b137402f50d8
mat-ben <matous.benedikt@gmail.com>
2026-04-19 19:55:51 +0200

    feat: add admin dashboard controller and template stub

--- src/main/java/cz/ufol/app/admin/AdminDashboardController.java
+++ src/main/java/cz/ufol/app/admin/AdminDashboardController.java
@@ -1,4 +1,17 @@
 package cz.ufol.app.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminDashboardController {
+
+    @GetMapping
+    public String helloAdmin(Model model) {
+        model.addAttribute("message", "Hello Admin!");
+        return "admin/dashboard";
+    }
 }
